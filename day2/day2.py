211068af935d41e49d92fe0ea11e321f615ec7b3
kkeroo <jasko10.kerec@gmail.com>
2022-12-02 15:53:54 +0100

    Finished day 2.

--- day2/day2.py
+++ day2/day2.py
@@ -1,9 +1,10 @@
 import sys
 
 
-def main(filename: str) -> None:
+def problem1(filename: str) -> None:
     f = open(filename)
     lines = f.readlines()
+    f.close()
 
     beating_table = {'X': 'C', 'Y': 'A', 'Z': 'B'}  # ex. Rock X beats scissors C
     scoring_table = {'X': 1, 'Y': 2, 'Z': 3}  # Rock is worth 1 point
@@ -24,9 +25,43 @@ def main(filename: str) -> None:
         total_score += game_score
     print(f'Total score: {total_score}')
 
+
+def problem2(filename: str) -> None:
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+
+    beating_table = {'A': 'C', 'B': 'A', 'C': 'B'}
+    scoring_table = {'A': 1, 'B': 2, 'C': 3}  # Rock is worth 1 point
+
+    total_score = 0
+    for line in lines:
+        game = line.split(' ')
+        game[-1] = game[-1][:-1] if game[-1][-1] == '\n' else game[-1]
+        opponent = game[0]
+        result = game[1]
+        if result == 'X':  # lose
+            play = beating_table[opponent]
+            total_score += scoring_table[play]
+        elif result == 'Y':  # draw
+            total_score += scoring_table[opponent] + 3
+        else:  # win
+            play = [k for (k,v) in beating_table.items() if v == opponent][0]
+            total_score += scoring_table[play] + 6
+
+    print(f'Total score: {total_score}')
+
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Run the script as: day2.py <filename>')
+    if len(sys.argv) != 3:
+        print('Run the script as: day2.py <problem> <filename>')
         exit(0)
-    filename = sys.argv[1]
-    main(filename)
+    try:
+        problem = int(sys.argv[1])
+        filename = sys.argv[2]
+        if problem == 1:
+            problem1(filename)
+        elif problem == 2:
+            problem2(filename)
+    except:
+        print('Run the script as: day2.py <problem> <filename>')
+        exit(0)
